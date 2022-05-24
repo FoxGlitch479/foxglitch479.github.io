5e684b1b9290428714cd08cdca424057b0e5453f
FoxGlitch479 <105945035+FoxGlitch479@users.noreply.github.com>
2022-05-24 09:15:21 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,1 +1,5 @@
-window.location.replace("https://httpbin.org/ip");
+//    http://127.0.0.1:5500/index.html
+//    https://httpbin.org/ip
+
+
+window.location.replace("https://whatismyipaddress.com");
